ade630b5158c909bc16c9f894b60a3093ac64c2d
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-07-07 14:41:35 +0200

    Add render cards in library func

--- script.js
+++ script.js
@@ -2,6 +2,42 @@ class Library {
   constructor() {
     this.bookArray = [];
   }
+
+  fillCard(book) {
+    const titleBox = document.createElement("div");
+    titleBox.textContent = book.title;
+    titleBox.className = "title";
+
+    const authorBox = document.createElement("div");
+    authorBox.textContent = book.author;
+
+    const pagesBox = document.createElement("div");
+    pagesBox.textContent = book.pages + " " + "pages";
+
+    const readBox = document.createElement("div");
+    readBox.textContent = book.read ? "Read" : "Not Read";
+
+    const idNumBox = document.createElement("div");
+    idNumBox.textContent = book.idNum;
+
+    return [titleBox, authorBox, pagesBox, readBox, idNumBox];
+  }
+
+  createCard(book) {
+    const library = document.getElementById("library");
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const cardContents = this.fillCard(book);
+    cardContents.forEach((content) => card.appendChild(content));
+
+    library.appendChild(card);
+  }
+
+  renderCards() {
+    this.bookArray.forEach((book) => this.createCard(book));
+  }
 }
 
 class Book {
@@ -28,5 +64,7 @@ const book2 = new Book("The Game of Thrones", "George R.R. Martin", 694, false);
 const library = new Library();
 
 library.bookArray.push(book1);
+library.bookArray.push(book2);
 
-console.log(library);
+library.renderCards();
+// ---
